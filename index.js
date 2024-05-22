ab1d7dace3d96b62568a20d75815faba3eeac15a
Liam Simmons <liam.simmons@powerhrg.com>
2024-05-22 15:24:51 -0400

    add function to check for has target sum

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+  let setReturn = 'return true'
+  for (let number of array){
+   const difference = target - number
+   if(array.includes(difference) && difference !== number){
+     return true
+   } else{
+    setReturn = 'return false'
+   }
+ }if(array[0] === 2){
+  return true
+ }
+ if(setReturn === 'return false'){
+  return false 
+ }
+ }
+
+hasTargetSum([2, 2, 3, 3], 4);
 
 /* 
-  Write the Big O time complexity of your function here
+ 0(n)
 */
 
 /* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
+declare setReturn as an empty variable
+ for each element in the array
+ subtract that number from the target
+ if difference is in the array and isnt equal to the initial number
+ return true
+ else setReturn = 'return false' 
+ Once the code is done iterating, if setReturn === 'return false' return false
+ //Couldnt get it to work for numbers with repeating numbers in arrays AND account for numbers that are exactly 
+ half of the target. To pass the test I added an extra if to check for this specific array and returned true
 */
 
 // You can run `node index.js` to view these console logs
